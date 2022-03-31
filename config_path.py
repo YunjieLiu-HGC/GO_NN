361940c02e3b74c4e5ebe6a15aaf11946c24ef3b
YunjieLiu-HGC <102719917+YunjieLiu-HGC@users.noreply.github.com>
2022-03-31 20:11:55 +0900

    Add files via upload

---
+++ config_path.py
@@ -0,0 +1,17 @@
+from os.path import join, realpath, dirname
+
+BASE_PATH = dirname(realpath(__file__))
+DATA_PATH = join(BASE_PATH, 'data')
+GENE_PATH = join(DATA_PATH, 'genes')
+PATHWAY_PATH = join(DATA_PATH, 'pathways')
+REACTOM_PATHWAY_PATH = join(PATHWAY_PATH, 'Reactome')
+PROSTATE_DATA_PATH = join(DATA_PATH, 'prostate')
+DIABETES_DATA_PATH = join(DATA_PATH, 'Chi')
+LC_DATA_PATH = join(DATA_PATH, 'Liver')
+MAP_DATA_PATH = join(DATA_PATH, 'maps')
+RUN_PATH = join(BASE_PATH, 'train')
+LOG_PATH = join(BASE_PATH, 'go_logs_dia_1p_5cv')
+PROSTATE_LOG_PATH = join(LOG_PATH, 'p1000')
+PARAMS_PATH = join(RUN_PATH, 'params')
+POSTATE_PARAMS_PATH = join(PARAMS_PATH, 'P1000')
+PLOTS_PATH = join(BASE_PATH, 'go_plot_dia_1p_5cv')
